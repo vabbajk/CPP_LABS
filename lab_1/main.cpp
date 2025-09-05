079d23a7cd14ac9451974ca38e1189cdeda4d12f
Vabbaj <vabbaj.n@gmail.com>
2025-09-05 06:53:38 +0300

    First lag started

---
+++ lab_1/main.cpp
@@ -0,0 +1,4 @@
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
