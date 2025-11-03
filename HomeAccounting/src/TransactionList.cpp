e184b908b3a0f0be804405b9689c316c8a180b9b
Vabbaj <vabbaj.n@gmail.com>
2025-11-03 23:01:55 +0300

    Непонял задание чутка, переделал

--- HomeAccounting/src/TransactionList.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/TransactionList.h"
-
-TransactionList::TransactionList() : dbFilename("") {
-
-}
-
-TransactionList::TransactionList(const std::string& dbFilename) : dbFilename(dbFilename) {
-    loadFromDatabase();
-}
